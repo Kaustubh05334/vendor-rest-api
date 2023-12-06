c3fe812e7ae1ad139f4361bd827e3bc576e39963
Kaustubh Bhargava <78357870+Kaustubh05334@users.noreply.github.com>
2023-12-06 22:46:09 +0530

    Update client_perform.py

--- client/vendor/client_perform.py
+++ client/vendor/client_perform.py
@@ -2,7 +2,7 @@ import requests
 
 
 vendor_id = input("Enter vendor id: ")
-url = f'http://127.0.0.1:8000/api/vendors/{vendor_id}/performance'
+url = f'http://127.0.0.1:8000/api/vendors/{vendor_id}/performance/'
 
 response = requests.get(url)
 
